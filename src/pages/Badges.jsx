ccfdacd58a5fe8c482b6f5530f333630388a7eff
rjfm <javier.fuentesm@hotmail.com>
2019-12-09 00:44:34 -0600

    Se agrega Loading Page

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -1,59 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BadgesList from "../components/BadgesList";
+import PageLoading from "./PageLoading";
 import confLogo from "../images/badge-header.svg";
+import api from "../api";
 import "./styles/Badges.css";
 const Badges = () => {
   const [data, setData] = useState({
-    data: [
-      {
-        id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-        firstName: "Freda",
-        lastName: "Grady",
-        email: "Leann_Berge@gmail.com",
-        jobTitle: "Legacy Brand Director",
-        twitter: "FredaGrady22221-7573",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-      },
-      {
-        id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-        firstName: "Major",
-        lastName: "Rodriguez",
-        email: "Ilene66@hotmail.com",
-        jobTitle: "Human Research Architect",
-        twitter: "MajorRodriguez61545",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-      },
-      {
-        id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-        firstName: "Daphney",
-        lastName: "Torphy",
-        email: "Ron61@hotmail.com",
-        jobTitle: "National Markets Officer",
-        twitter: "DaphneyTorphy96105",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-      }
-    ]
+    data: undefined,
+    error: null,
+    loading: true
   });
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () => {
+    try {
+      const response = await api.badges.list();
+      setData({ ...data, data: response, loading: false });
+    } catch (error) {
+      console.log(error);
+      setData({ ...data, loading: false, error: error });
+      console.log(data);
+    }
+  };
 
   return (
     <>
-      <div className="Badges">
-        <div className="Badges__hero">
-          <div className="Badges__container">
-            <img className="Badges__conf-logo" src={confLogo} alt="Conf Logo" />
+      {data.loading ?(
+        <PageLoading/>
+      ) : (
+        <>
+          <div className="Badges">
+            <div className="Badges__hero">
+              <div className="Badges__container">
+                <img
+                  className="Badges__conf-logo"
+                  src={confLogo}
+                  alt="Conf Logo"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
- 
 
-      <div className="Badges__list">
-        <div className="Badges__container">
-          <BadgesList data={data} />
-        </div>
-      </div>
+          <div className="Badges__list">
+            <div className="Badges__container">
+              <BadgesList data={data} />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
